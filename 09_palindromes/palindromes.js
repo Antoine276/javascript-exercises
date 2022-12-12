832b6e24b14b2d3b994e013fa859becb4af98ae0
Antoine276 <antoine.colombo276@gmail.com>
2022-12-12 20:41:21 +0100

    Array Exercices

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let array = string.toLowerCase().split("").filter((char) => (char.toLowerCase() !== char.toUpperCase()));
 
+    for (let i = 0; i < (array.length / 2); i++) {
+        if (array[i] !== array[(array.length - 1) - i]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(param) {
+    let array = [1, 1];
+    const number = Number(param);
 
+    if (number < 1 || number === NaN) {
+        return "OOPS";
+    }
+    else if (number === 1) {
+        return array[0];
+    }
+
+    for (i = 2; i < number; i++) {
+        let tmp = array[0] + array[1]
+        
+        array[0] = array[1];
+        array[1] = tmp;
+    }
+
+    return array[1];
 };
 
 // Do not edit below this line
